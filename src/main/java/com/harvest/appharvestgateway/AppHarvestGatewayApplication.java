c07f2b8358d307bd755eef02d59fa272083c9127
someliukh <stepan.omeliukh.work@gmail.com>
2024-06-21 23:52:36 +0300

    Init commit

---
+++ src/main/java/com/harvest/appharvestgateway/AppHarvestGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.harvest.appharvestgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHarvestGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppHarvestGatewayApplication.class, args);
+    }
+
+}
